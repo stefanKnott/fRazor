a8b8f0b32d7d09847c142e301a5d758b50e8a535
stefanKnott <stefan.knott@layer3tv.com>
2016-09-07 16:10:45 -0600

    change in return values

--- fRazor.go
+++ fRazor.go
@@ -11,68 +11,69 @@ var rzScalar int64
 
 func razor(f *os.File) (string, error) {
 	fmt.Println("razoring")
+	retStr := "hit error" //will reset to valid resizing str if we dont hit any errors...
 
 	fstats, err := f.Stat()
 	if err != nil {
-		panic(err)
+		return retStr, err
 	}
 	numBytes := fstats.Size()
 
 	fi, err := f.Stat()
 	if err != nil {
-		return "nil", err
+		return retStr, err
 	}
 	buf := bytes.NewBuffer(make([]byte, 0, fi.Size()))
 
 	_, err = f.Seek(0, os.SEEK_SET)
 	if err != nil {
-		return "nil", err
+		return retStr, err
 	}
 	_, err = io.Copy(buf, f)
 	if err != nil {
-		return "nil", err
+		return retStr, err
 	}
 
 	var bytesRazord int64
 	bytesRazord = 0
 	numLinesRazord := 0
-	//remove lines until we have removed 1/rzScalar of file..
+	//remove lines until we have removed ~1/rzScalar of file..
 	for {
 		line, err := buf.ReadString('\n')
 		if err != nil && err != io.EOF {
-			return "nil", err
+			return retStr, err
 		}
 		numLinesRazord++
 		bytesRazord += int64(len(line))
 		if bytesRazord >= numBytes/rzScalar {
-			fmt.Printf("RESIZING COMPLETED!\nRemoved: %v bytes from file with size of %v\n", bytesRazord, numBytes)
-			break
+			retStr = fmt.Sprintf("Resizing completed!\nRemoved: %v bytes from file with size of %v\n", bytesRazord, numBytes)
+			return retStr, nil
 		}
 
 	}
 	_, err = f.Seek(0, os.SEEK_SET)
 	if err != nil {
-		return "nil", err
+		return retStr, err
 	}
 	nw, err := io.Copy(f, buf)
 	if err != nil {
-		return "nil", err
+		return retStr, err
 	}
 	err = f.Truncate(nw)
 	if err != nil {
-		return "nil", err
+		return retStr, err
 	}
 	err = f.Sync()
 	if err != nil {
-		return "nil", err
+		return retStr, err
 	}
 
 	_, err = f.Seek(0, os.SEEK_SET)
 	if err != nil {
-		return "nil", err
+		return retStr, err
 	}
 
-	return "completed", nil
+	return retStr, nil
 }
 
 func Razor(fptr *os.File) {
