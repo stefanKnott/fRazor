82698355a3745820aef654c22f365592deca1c1e
stefanKnott <stefan.knott@layer3tv.com>
2016-09-08 09:48:00 -0600

    changed exit logic

--- fRazor.go
+++ fRazor.go
@@ -7,32 +7,30 @@ import (
 	"os"
 )
 
-var rzScalar int64
+var rzScalar float64
 
-func razor(f *os.File) (string, error) {
-	fmt.Println("razoring")
-	retStr := "hit error" //will reset to valid resizing str if we dont hit any errors...
-
-	fstats, err := f.Stat()
+func checkErr(err error) {
 	if err != nil {
-		return retStr, err
+		panic(err)
 	}
+}
+
+func razor(f *os.File) (*os.File, error) {
+	fstats, err := f.Stat()
+	checkErr(err)
+
 	numBytes := fstats.Size()
 
 	fi, err := f.Stat()
-	if err != nil {
-		return retStr, err
-	}
+	checkErr(err)
+
 	buf := bytes.NewBuffer(make([]byte, 0, fi.Size()))
 
 	_, err = f.Seek(0, os.SEEK_SET)
-	if err != nil {
-		return retStr, err
-	}
+	checkErr(err)
+
 	_, err = io.Copy(buf, f)
-	if err != nil {
-		return retStr, err
-	}
+	checkErr(err)
 
 	var bytesRazord int64
 	bytesRazord = 0
@@ -40,47 +38,51 @@ func razor(f *os.File) (string, error) {
 	//remove lines until we have removed ~1/rzScalar of file..
 	for {
 		line, err := buf.ReadString('\n')
-		if err != nil && err != io.EOF {
-			return retStr, err
+		if err == io.EOF {
+			//exit on the last line
+			fmt.Println("EOF")
+			break
 		}
+		checkErr(err)
+
+		fmt.Println("REMOVING: ", line)
 		numLinesRazord++
 		bytesRazord += int64(len(line))
-		if bytesRazord >= numBytes/rzScalar {
-			retStr = fmt.Sprintf("Resizing completed!\nRemoved: %v bytes from file with size of %v\n", bytesRazord, numBytes)
-			return retStr, nil
+
+		//we have removed the amount we need..so seek file and break from loop
+		if float64(bytesRazord) >= float64(numBytes)*rzScalar {
+			break
 		}
 
 	}
+
 	_, err = f.Seek(0, os.SEEK_SET)
-	if err != nil {
-		return retStr, err
-	}
+	checkErr(err)
+
 	nw, err := io.Copy(f, buf)
-	if err != nil {
-		return retStr, err
-	}
+	checkErr(err)
+
 	err = f.Truncate(nw)
-	if err != nil {
-		return retStr, err
-	}
+	checkErr(err)
+
 	err = f.Sync()
-	if err != nil {
-		return retStr, err
-	}
+	checkErr(err)
 
-	_, err = f.Seek(0, os.SEEK_SET)
-	if err != nil {
-		return retStr, err
-	}
+	//seek to end of file so our file pointer will be writing in the proper place
+	_, err = f.Seek(fstats.Size(), os.SEEK_SET)
+	checkErr(err)
 
-	return retStr, nil
+	fmt.Printf("Resizing completed!\nRemoved: %v bytes from file with size of %v\n", bytesRazord, numBytes)
+	return f, nil
 }
 
-func Razor(fptr *os.File) {
-	rzScalar = 10
-	str, err := razor(fptr)
+//we return the fptr passed in so the outside program writing to the file is pointed to the proper, updated line in memory after line removal
+func Razor(fptr *os.File, scalar float64) *os.File {
+	rzScalar = scalar
+	fptr, err := razor(fptr)
 	if err != nil {
-		fmt.Println(str)
 		panic(err)
 	}
+
+	return fptr
 }
