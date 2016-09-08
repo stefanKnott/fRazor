b882c0cc5df4d46ff0f501895b36398ced5c39a1
stefanKnott <stefan.knott@layer3tv.com>
2016-09-08 09:48:20 -0600

    attempt write after file ptr is returned

--- main.go
+++ main.go
@@ -6,10 +6,14 @@ import (
 
 func main() {
 	fptr, err := os.OpenFile("test.txt", os.O_RDWR|os.O_CREATE, 0666)
+	//write to beginning of file - note that this should be removed via fRazor
+	fptr.WriteString("OMG THIS FILE HAS BEEN OPENED WHO DID THIS?")
 	if err != nil {
 		panic(err)
 	}
 	defer fptr.Close()
 
-	Razor(fptr)
+	fptr = Razor(fptr, .5)
+	//write to end -- the fptr will be directed at EOF upon finishing Razor
+	fptr.WriteString("\nIF THIS ISNT AT THE END OF THE FILE IM GOING TO FREAK")
 }
